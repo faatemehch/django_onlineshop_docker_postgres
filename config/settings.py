c6e374a81c9da265ac23790f73de3a2374f5ede0
Fch <faatemehchamani@Fchs-MacBook-Pro.local>
2024-01-17 09:45:42 +0330

    add psycopg2

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
-        'PORT': 5432
+        'PORT': 5432,
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
     }
