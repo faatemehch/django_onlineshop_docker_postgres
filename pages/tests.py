2bb05201f72abac0b9b804344ae8e95e15a47eb2
Fch <faatemehchamani@Fchs-MacBook-Pro.local>
2024-01-17 11:40:48 +0330

    add pages model test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class HomeAndAboutTest(TestCase):
+    def test_home_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_url_by_name(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
