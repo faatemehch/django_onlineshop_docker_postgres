14fa66e336caf41823abab016c592db8a072df37
Fch <faatemehchamani@Fchs-MacBook-Pro.local>
2024-01-24 09:20:36 +0330

    product admin updated

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Product, Comment
 
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ('author',
+              'text',
+              'points',
+              'active',)
+    extra = 0 # To remove empty records
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'active')
+    inlines = (CommentsInline, )
 
 
 @admin.register(Comment)
