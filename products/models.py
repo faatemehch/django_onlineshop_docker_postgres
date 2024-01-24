6640285e79592ac0f994a9f94c24826b2f848968
Fch <faatemehchamani@Fchs-MacBook-Pro.local>
2024-01-24 09:09:11 +0330

    comment filter tags added

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+
+
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -15,7 +17,13 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"pk": self.pk})
-    
+
+
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
+
 class Comment(models.Model):
     CHOICES = (
         ('1', 'Very Bad'),
@@ -24,18 +32,22 @@ class Comment(models.Model):
         ('4', 'Good'),
         ('5', 'Perfect'),
     )
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetiem_modified = models.DateTimeField(auto_now=True)
     points = models.CharField(choices=CHOICES)
     active = models.BooleanField(default=True)
 
+    # Manager
+    objects = models.Manager()
+    active_commnets_manager = ActiveCommentsManager()
+
     def __str__(self):
         return str(self.product)
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"pk": self.product.id})
-    
-    
