94bb1de588dd12a95971e21382f9fb88c3f3cde4
Fch <faatemehchamani@Fchs-MacBook-Pro.local>
2024-02-02 10:20:07 +0330

    product image added

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,5 @@ urlpatterns = [
 
     # rosetta (i18n)
     path('rosetta/', include('rosetta.urls'))
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -3,14 +3,18 @@ from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+
 class Product(models.Model):
     title = models.CharField(max_length=200, verbose_name=_('product title'))
     description = models.TextField(verbose_name=_('description'))
-    datetiem_created = models.DateTimeField(auto_now_add=True, verbose_name=_('created date'))
-    datetiem_modified = models.DateTimeField(auto_now=True, verbose_name=_('last modified date'))
+    datetiem_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('created date'))
+    datetiem_modified = models.DateTimeField(
+        auto_now=True, verbose_name=_('last modified date'))
     price = models.PositiveIntegerField(default=0, verbose_name=_('price'))
     active = models.BooleanField(default=1, verbose_name=_('Active\Inactive'))
-    # image = models.ImageField()
+    image = models.ImageField(verbose_name=_("Product Image"), upload_to='product/product_image', blank=True)
+
 
     def __str__(self):
         return str(self.title)
@@ -36,12 +40,15 @@ class Comment(models.Model):
         Product, on_delete=models.CASCADE, related_name='comments', verbose_name=_('product'))
     author = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, related_name='comments', verbose_name=_('author text'))
-    text = models.TextField( verbose_name=_('message text'))
-    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('created date'))
-    datetiem_modified = models.DateTimeField(auto_now=True, verbose_name=_('modified date'))
+    text = models.TextField(verbose_name=_('message text'))
+    datetime_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('created date'))
+    datetiem_modified = models.DateTimeField(
+        auto_now=True, verbose_name=_('modified date'))
     points = models.CharField(choices=CHOICES, verbose_name=_('Points'))
-    active = models.BooleanField(default=True, verbose_name=_('Active \ Inactive'))
-
+    active = models.BooleanField(
+        default=True, verbose_name=_('Active \ Inactive'))
+    
     # Manager
     objects = models.Manager()
     active_commnets_manager = ActiveCommentsManager()
